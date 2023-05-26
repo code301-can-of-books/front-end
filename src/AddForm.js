1163eb94fc92269a14ac0e3e7c475b4b5edd5c66
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-05-25 21:24:53 -0700

    :rocket: Front end up & running

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,50 +1,59 @@
-import React from "react";
-import { Button } from "react-bootstrap";
-import { Form } from "react-bootstrap";
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 
 class AddForm extends React.Component {
-
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target)
+    console.log(e.target);
     const newBook = {
       title: e.target.title.value,
       description: e.target.description.value,
-      status: e.target.status.value
-    }
+      status: e.target.status.value,
+    };
     this.props.postBook(newBook);
-  }
-
+  };
 
   render() {
-
     return (
+      <>
         <Modal show={this.props.showModal}>
           <Modal.Header>{this.props.books_id}</Modal.Header>
           <Modal.Body>
             <Form onSubmit={this.handleSubmit}>
               <Form.Group controlId="title">
-                <Form.Label>Book Title</Form.Label>
-                <Form.Control type="text" placeholder="Book Title Goes Here..." />
+                <Form.Label>Book Title</Form.Label>s
+                <Form.Control
+                  type="text"
+                  placeholder="Book Title Goes Here..."
+                />
               </Form.Group>
 
               <Form.Group controlId="description">
-                <Form.Label >Book Description</Form.Label>
-                <Form.Control type="text" placeholder="Summarize The Book Here" />
+                <Form.Label>Book Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Summarize The Book Here"
+                />
               </Form.Group>
 
               <Form.Group controlId="status">
-                <Form.Label >Book Status</Form.Label>
-                <Form.Control type="text" placeholder="Have You Read The Book?" />
+                <Form.Label>Book Status</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Have You Read The Book?"
+                />
               </Form.Group>
 
-              <Button type="submit" onClick={this.props.hideModal}>Add Book</Button>
-
+              <Button type="submit" onClick={this.props.hideModal}>
+                Add Book
+              </Button>
             </Form>
           </Modal.Body>
         </Modal>
-    )
+      </>
+    );
   }
 }
 
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -10,13 +10,13 @@ class BestBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      showModal: false
+      showModal: false,
     };
   }
 
   hideModal = () => {
-    this.setState({showModal: false})
-  }
+    this.setState({ showModal: false });
+  };
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
   componentDidMount() {
@@ -27,34 +27,33 @@ class BestBooks extends React.Component {
     try {
       const response = await axios(`${process.env.REACT_APP_SERVER}/book`);
       console.log('API Response:', response);
-      this.setState({ books: response.data,});
+      this.setState({ books: response.data });
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   postBook = async (newBook) => {
     try {
-      const url = `${process.env.REACT_APP_SERVER}/book`
+      const url = `${process.env.REACT_APP_SERVER}/book`;
       const response = await axios.post(url, newBook);
-      this.setState({ books: [...this.state.books, response.data] }, () => console.log(this.state.books))
-      console.log(response.data)
-    }
-    catch (error) {
+      this.setState({ books: [...this.state.books, response.data] }, () =>
+        console.log(this.state.books)
+      );
+      console.log(response.data);
+    } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   deleteBook = async (deleteBook) => {
-    const url = `${process.env.REACT_APP_SERVER}/book/${deleteBook._id}`
-    await axios.post(url);
-    const updatedBook = this.state.book.filter(book => book._id !== this.deleteBook._id);
-    this.setState({ books: updatedBook })
-
-  }
-
-
-
+    const url = `${process.env.REACT_APP_SERVER}/book/${deleteBook._id}`;
+    await axios.delete(url);
+    const updatedBook = this.state.books.filter(
+      (book) => book._id !== deleteBook._id
+    );
+    this.setState({ books: updatedBook });
+  };
 
   render() {
     /* TODO: render all the books in a Carousel */
@@ -64,43 +63,50 @@ class BestBooks extends React.Component {
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
         <div>
           {this.state.books.length > 0 ? (
-            <Carousel variant='dark' >
-              {this.state.books.map((book , idx) =>
-                <Carousel.Item key= {idx}>
+            <Carousel variant="dark">
+              {this.state.books.map((book, idx) => (
+                <Carousel.Item key={idx}>
                   <img
                     className="d-block w-100"
                     src={`https://placehold.co/600x400?text=${book.title}`}
-                    alt=''
+                    alt=""
                   />
                   <Carousel.Caption>
-                    <h4>
-                      Status:{book.status}
-                    </h4>
+                    <h4>Status:{book.status}</h4>
                     <p>{book.description}</p>
+                    <Button onClick={() => this.deleteBook(book)}>
+                      Delete Book
+                    </Button>
                   </Carousel.Caption>
                 </Carousel.Item>
-              )}
+              ))}
             </Carousel>
-          ) : (<Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src={`https://placehold.co/600x400?text=No+Books+Found`}
-                alt='No Books Found'
-              />
-            </Carousel.Item>
-          </Carousel>
+          ) : (
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={`https://placehold.co/600x400?text=No+Books+Found`}
+                  alt="No Books Found"
+                />
+              </Carousel.Item>
+            </Carousel>
           )}
         </div>
-        <Button onClick={() => this.setState({showModal: true})}>Add Your Book</Button>
+        <Button onClick={() => this.setState({ showModal: true })}>
+          Add Your Book
+        </Button>
         <div>
-          <AddForm postBook={this.postBook} 
+          <AddForm
+            postBook={this.postBook}
             showModal={this.state.showModal}
-            hideModal={this.hideModal}/>
-
+            hideModal={this.hideModal}
+            books={this.state.books}
+            deleteBook={this.deleteBook}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
